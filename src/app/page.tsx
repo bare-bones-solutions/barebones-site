47c8c098ac1a9ae38380d03f40ce60a26806c1f9
ttieman <tiemantanner@gmail.com>
2025-09-12 23:35:11 -0700

    adjusted styles to mobile first centric styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
       <Header/>
       <main className="flex flex-col gap-[32px] row-start-2 items-center w-full">
           <Hero />
+          
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
 
