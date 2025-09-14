6c197b192ed275ac542da04b75d550b2b71f76bf
ttieman <tiemantanner@gmail.com>
2025-09-14 14:56:22 -0700

    refactor badge section

--- src/components/homepage/technologies.tsx
+++ src/components/homepage/technologies.tsx
@@ -1,52 +1,48 @@
 import { Badge } from "@/components/ui/badge";
 
+const badges = [
+    {
+        title: "React"
+    },
+    {
+        title: "Node.js"
+    },
+    {
+        title: "TypeScript"
+    },
+    {
+        title: "Python"
+    },
+    {
+        title: "PostgresSQL"
+    },
+    {
+        title: "Azure"
+    },
+    {
+        title: "Docker"
+    },
+    {
+        title: "GraphQL"
+    },
+]
+
 export default function Technologies() {
     return (
         <div className="mt-[82px] flex flex-col px-5 text-center">
             <div className="mb-[24px]">
                 <h2 className="text-2xl font-bold">Technologies We Love</h2>
             </div>
-            <div className="flex flex-row flex-wrap items-center justify-center gap-[16px]">
-                <Badge variant="secondary">
-                    <span className="py-2 px-3 text-[16px]">
-                        React
-                    </span>
-                </Badge>
-                <Badge variant="secondary">
-                    <span className="py-2 px-3 text-[16px]">
-                        Node.js
-                    </span>
-                </Badge>
-                <Badge variant="secondary">
-                    <span className="py-2 px-3 text-[16px]">
-                        TypeScript
-                    </span>
-                </Badge>
-                <Badge variant="secondary" >
-                    <span className="py-2 px-3 text-[16px]">
-                        Python
-                    </span>
-                </Badge>
-                <Badge variant="secondary" >
-                    <span className="py-2 px-3 text-[16px]">
-                        PostgresSQL
-                    </span>
-                </Badge>
-                <Badge variant="secondary" >
-                    <span className="py-2 px-3 text-[16px]">
-                        Azure
-                    </span>
-                </Badge>
-                <Badge variant="secondary" >
-                    <span className="py-2 px-3 text-[16px]">
-                        Docker
-                    </span>
-                </Badge>
-                <Badge variant="secondary" >
-                    <span className="py-2 px-3 text-[16px]">
-                        GraphQL
-                    </span>
-                </Badge>
+            <div className="flex flex-row flex-wrap items-center justify-center gap-[16px] container">
+            {
+                badges.map((b) => (
+                    <Badge key={b.title} variant="secondary">
+                        <span className="py-2 px-3 text-[16px]">
+                            {b.title}
+                        </span>
+                    </Badge>
+                ))
+            }
             </div>
         </div >
     );
