3f5102f94b76cbac5729de7dce544158426b10c3
ttieman <tiemantanner@gmail.com>
2025-09-14 12:59:23 -0700

    updated desktop format of hero section

--- src/components/homepage/hero.tsx
+++ src/components/homepage/hero.tsx
@@ -5,14 +5,15 @@ export default function Hero() {
     return (
         <div className="max-w-3xl mt-[204px] px-5 flex flex-col items-center justify-around h-[300px]">
             <div className="flex flex-col items-center font-bold">
-                <h1 className="text-3xl">Software Solutions</h1>
-                <h1 className="text-3xl">Without the Bloat</h1>
+                <h1 className="text-3xl sm:text-4xl md:text-5xl">Software Solutions</h1>
+                <h1 className="text-3xl sm:text-4xl md:text-5xl">Without the Bloat</h1>
             </div>
             <div className="flex flex-col items-center justify-center">
-                <p className="text-center max-w-[600px]">We strip away the unnecessary to deliver clean, efficient, and powerful software solutions. From concept to deployment, we focus on what matters most - functionality that works.
+                <p className="text-center max-w-[700px] sm:text-lg">
+                    We strip away the unnecessary to deliver clean, efficient, and powerful software solutions. From concept to deployment, we focus on what matters most - functionality that works.
                 </p>
             </div>
-            <div className="flex flex-col justify-center gap-[14px] mt-[24px]">
+            <div className="flex flex-col justify-center gap-[14px] mt-[24px] sm:flex-row">
                 <Button>Start Your Project</Button>
                 <Button>View Our Work</Button>
             </div>
