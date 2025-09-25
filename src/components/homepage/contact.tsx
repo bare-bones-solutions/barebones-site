1f7f9b514774ae5a26cd75b9761d8b82d11434e1
ttieman <tiemantanner@gmail.com>
2025-09-24 18:18:09 -0700

    update text on contact section...I am not in love with it though  but hey its a commit

--- src/components/homepage/contact.tsx
+++ src/components/homepage/contact.tsx
@@ -5,7 +5,7 @@ export default function Contact() {
         <div className="px-5 mt-[82px] text-center">
             <div className="flex flex-col gap-[32px]">
                 <h2 className="">Read to Get Started?</h2>
-                <p>Let's discuss your project and build something amazing together.</p>
+                <p>Reach out today—your next breakthrough starts with a conversation.</p>
                 <div>
                     <Button className="hover:scale-105 hover:ring-2 hover:ring-bone-accent hover:bg-earth-light hover:transition-all hover:duration-200">Get in Touch</Button>
                 </div>
